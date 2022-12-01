c08943530cd9d46d2da1fe9f1ece019871328559
MXF88 <3103236692@qq.com>
2022-12-01 20:03:17 +0800

    项目创建

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <el-container>
+    <el-header>后台管理系统</el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu router v-for="(item,index) in this.$router.options.routes" :key="index">
+          <el-submenu index="1" v-if="item.children">
+            <template slot="title"><i class="el-icon-s-order"></i>{{item.name}}</template>
+              <el-menu-item :index="children.path" v-for="(children,indexj) in item.children" :key="indexj">{{children.name}}</el-menu-item>
+            </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-container>
+  </el-container>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Test1.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Test1
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Test1'
+}
+</script>
+
+<style scoped>
+
+</style>
